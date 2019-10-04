eb32ad88fdd2d02e0d58c34101a5f529edeec394
susanna <susannamichael54@gmail.com>
2019-10-04 14:43:44 +0530

    Heap implementation

---
+++ algorithms/Heap.java
@@ -0,0 +1,101 @@
+import java.util.*;
+import java.util.stream.*;
+
+public class Heap {
+
+	public Heap (int maxSize) {
+		this.maxSize = maxSize;
+		this.size = 0;
+		this.heap = new int[maxSize];
+	}
+
+	private int size;
+
+	private int maxSize;
+
+	private int[] heap;
+
+	public int size () {
+		return this.size;
+	}
+
+	public int maxSize () {
+		return this.maxSize;
+	}
+
+	private void maxHeapify (int index) {
+		int largestIndex = index;
+		int leftIndex = 2 * index + 1;
+		int rightIndex = 2 * index + 2;
+		if(leftIndex < this.size - 1 && this.heap[leftIndex] > this.heap[largestIndex]) {
+			largestIndex = leftIndex;
+		}
+		if(rightIndex < this.size - 1 && this.heap[rightIndex] > this.heap[largestIndex]) {
+			largestIndex = rightIndex;
+		}
+
+		if(index != largestIndex){
+			swap(index, largestIndex);
+			maxHeapify(largestIndex);
+		}
+
+	}
+
+	private void swap (int index1, int index2) {
+		int temp = this.heap[index1];
+		this.heap[index1] = this.heap[index2];
+		this.heap[index2] = temp;
+	}
+
+	private void buildMaxHeap (){
+		for(int i = (this.size - 1) / 2; i <= 0; i--) {
+			maxHeapify(i);
+		}
+	}
+
+	private int parent(int index) {
+		return (index - 1) / 2;
+	}
+
+	public void insert(int value) {
+		size++;
+		if(size > maxSize) {
+			return;
+		}
+		heap[size - 1] = value;
+		int currentIndex = size - 1;
+		while(heap[currentIndex] > heap[parent(currentIndex)]) {
+			swap(currentIndex, parent(currentIndex));
+			currentIndex = parent(currentIndex);
+		}		
+	}
+
+	public void print() {
+		String output = "";
+		for(int i = 0; i < size; i++) {
+			output += heap[i] + " ";
+		}
+		System.out.println(output);
+	}
+
+	public int remove() {
+		int popped = heap[0];
+		size--;
+		maxHeapify(size - 1);
+		return popped;
+	}
+
+	public static void main (String [] args) {
+		Heap heap = new Heap(7);
+		heap.insert(5);
+		heap.insert(1);
+		heap.insert(6);
+		heap.insert(2);
+		heap.insert(3);
+		heap.insert(10);
+		heap.print();
+		heap.remove();
+		heap.print();
+	}
+
+}
