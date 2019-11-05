b2fa6415917f56e2f36ce59ffa8771916437bf9b
Susanna Michael <smic@Susannas-MacBook-Air.local>
2019-11-05 12:07:47 +0530

    Doubly linked list and linked list implementation

---
+++ algorithms/DoublyLinkedList.java
@@ -0,0 +1,106 @@
+import java.util.*;
+
+public class DoublyLinkedList {
+
+	Link head;
+	Link tail;
+
+	private class Link {
+		Link (String str) {
+			this.str = str;
+		}
+	    Link next;
+	    Link previous;
+		String str;
+	}
+
+	public void insert(String str) {
+		Link link = new Link(str);
+		if(null != this.head) {
+			this.tail.next = link;
+			link.previous = this.tail;
+			this.tail = link;
+		} else {
+			this.head = link;
+			this.tail = link;
+		}	
+	}
+
+	public void print() {
+		Link l = this.head;
+		while (null != l) {
+			System.out.print(l.str + " ");
+			l = l.next;
+		}
+	}
+
+	public void delete (String str) {
+		Link l = this.head;
+		while(null != l) {
+			if(l.str.equals(str)) {
+				if(null != l.previous) {
+					if(null != l.next) {
+						l.previous.next = l.next;
+						l.next.previous = l.previous;
+					} else {
+						l.previous.next = null;
+						this.tail = l.previous;
+					}
+				} else {
+					l.next.previous = null;
+					this.head = l.next;
+				}
+				l = null;
+				break;
+			}
+			l = l.next;
+		}
+	}
+
+	public String find(String str) {
+		Link l = this.head;
+		String output = null;
+		while(null != l) {
+			if(l.str.equals(str)) {
+				output = l.str;
+				break;
+			}
+			l = l.next;
+		}
+		return output;
+	}
+
+	private static Scanner scan = new Scanner(System.in);
+
+	public static void main (String [] args) {
+		DoublyLinkedList doublyLinkedLst = new DoublyLinkedList();
+		System.out.println("Enter the number of Strings to be inserted in the list: ");
+		int n = scan.nextInt();
+		System.out.println("Enter the Strings to be inserted: ");
+		scan.nextLine();
+		for(int i = 0; i < n; i++) {
+			doublyLinkedLst.insert(scan.nextLine());
+		}
+		System.out.println("The linked list is: ");
+		doublyLinkedLst.print();
+		deleteElements(n, doublyLinkedLst);
+		System.out.println("The list after deletion: ");
+		doublyLinkedLst.print();
+	}
+
+	public static void deleteElements(int n, DoublyLinkedList doublyLinkedLst) {
+		System.out.println("\nEnter the number of elements to be deleted from the list: ");
+		int d = scan.nextInt();
+		if(d > n) {
+			System.out.println("Enter a number less than the size of the list: ");
+			deleteElements(n, doublyLinkedLst);
+		} else {
+			System.out.println("Enter the elements to be deleted:");
+			scan.nextLine();
+			for(int i = 0; i < d; i++) {
+				doublyLinkedLst.delete(scan.nextLine());
+			}
+		}
+			
+	}
+}
---
+++ algorithms/LinkedList.java
@@ -0,0 +1,99 @@
+import java.util.*;
+
+public class LinkedList {
+
+	Link head;
+	Link tail;
+
+	private class Link {
+		Link (String str) {
+			this.str = str;
+		}
+		Link next;
+		String str;
+	}
+
+	public void insert (String str) {
+		Link l = new Link(str);
+		if(null != this.head) {
+			this.tail.next = l;
+			this.tail =l;
+		} else {
+			this.head = l;
+			this.tail = l;
+		}
+	}
+
+	public void delete (String str) {
+		Link l = this.head;
+		while (null != l) {
+			if(str.equals(l.str)) {
+				if(this.head == l) {
+					this.head = l.next;
+				} else {
+					if(null == l.next) {
+						this.tail = findPrevious(l);
+					} else {
+						findPrevious(l).next = l.next;
+					}
+				}
+				l = null;
+				break;
+			}
+			l = l.next;
+		}
+	}
+
+	private Link findPrevious (Link l) {
+		Link link = this.head;
+		while (null != link) {
+			if(link.next == l) {
+				return link;
+			}
+			link = link.next;
+		}
+		return null;
+	}
+
+	public void print() {
+		Link l = this.head;
+		while (null != l) {
+			System.out.print(l.str + " ");
+			l = l.next;
+		}
+	}
+
+	private static Scanner scan = new Scanner(System.in);
+
+	public static void main (String [] args) {
+		LinkedList llst = new LinkedList();
+		System.out.println("Enter the number of strings to be inserted to the list: ");
+		int n = scan.nextInt();
+		System.out.println("Enter the strings to be inserted into the list:");
+		scan.nextLine();
+		for(int i = 0; i < n; i++) {
+			llst.insert(scan.nextLine());
+		}
+		System.out.println("The elements in the linked list are: ");
+		llst.print();
+		deleteElements(n, llst);
+		System.out.println("The list after deletion: ");
+		llst.print();
+	}
+
+	public static void deleteElements(int n, LinkedList llst) {
+		System.out.println("\nEnter the number of elements to be deleted from the list: ");
+		int d = scan.nextInt();
+		if(d > n) {
+			System.out.println("Enter a number less than the size of the list: ");
+			deleteElements(n, llst);
+		} else {
+			System.out.println("Enter the elements to be deleted: ");
+			scan.nextLine();
+			for(int i = 0; i < d; i++) {
+				llst.delete(scan.nextLine());
+			}
+		}
+			
+	}
+}
